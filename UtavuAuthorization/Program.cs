a4dec8f43922b6acec3fcc62deee1e3b38dd7cf8
Hariharasudhan177 <hari17792@gmail.com>
2024-10-24 10:50:37 +0100

    Fix di of the services related to signup api

--- UtavuAuthorization/Program.cs
+++ UtavuAuthorization/Program.cs
@@ -21,6 +21,10 @@ public class Program
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+        builder.Services.AddScoped<IGoogleAuthService, GoogleAuthService>();
+        builder.Services.AddScoped<IJwtService, JwtService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+
         builder.Services.AddControllers();
 
         builder.Services.AddEndpointsApiExplorer();
